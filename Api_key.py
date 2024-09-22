c141ad172ef82e74185e07b2056e474d379089c4
ShivaNaniSimha <shivananisimha@gmail.com>
2024-09-23 00:07:32 +0530

    api key added

--- Api_key.py
+++ Api_key.py
@@ -1,4 +1,4 @@
 #create the youtube data api v3 key from google cloud platform 
-youtube_api_key=""
+youtube_api_key="AIzaSyDjmzKgZfC5cRV3nuD9ZlZBDEoFb1neOik"
 #create the openai api key 
-chat_apikey=""
+gemini_api_key="AIzaSyCvBm7GCw-vx-1TVqMrHK04SMszkPMmY8M"
