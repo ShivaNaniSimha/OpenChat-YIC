41170054bfd60444109eaf4eb2977acdda16dc70
ShivaNaniSimha <shivananisimha@gmail.com>
2024-09-23 00:38:00 +0530

    changes made

--- key.py
+++ key.py
@@ -1,4 +1,4 @@
 #create the youtube data api v3 key from google cloud platform 
-youtube_api_key="AIzaSyDjmzKgZfC5cRV3nuD9ZlZBDEoFb1neOik"
+youtube_api_key="Put your api "
 #create the openai api key 
-gemini_api_key="AIzaSyCvBm7GCw-vx-1TVqMrHK04SMszkPMmY8M"
+gemini_api_key="Arrange Gemini Api Key"
